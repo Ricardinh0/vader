9889cd12210110bc4648652a09909b6e426ce5c3
Richard <richard@oneflare.com>
2017-04-19 09:21:46 +1000

    file load and paint

--- app/assets/javascripts/AvatarEditor/AvatarEditor.jsx
+++ app/assets/javascripts/AvatarEditor/AvatarEditor.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import HiDPICanvas from '../utils/HiDPICanvas';
 
 class AvatarEditor extends Component {
@@ -6,66 +6,109 @@ class AvatarEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      boundingBox: {}
-    };
+      imageSrc: ''
+    }
+    this.handleSave = this.handleSave.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleFileLoad = this.handleFileLoad.bind(this);
   }
-
-  getBoundingBox() {
-
-  }
-
-  setBoundingBox(obj) {
-    boundingBox.width = obj.width || boundingBox.width;
-    boundingBox.height = obj.height || boundingBox.height;
-    boundingBox.x = obj.x || boundingBox.x;
-    boundingBox.y = obj.y || boundingBox.y;
-  };
-
-  getAnchor(params) {
-    //
-    var params = params || {};
-    const boundingBox = getBoundingBox();
-    //
-    var x = params.x || boundingBox.x;
-    var y = params.y || boundingBox.y;
-    var width = params.width || boundingBox.width;
-    var height = params.height || boundingBox.height;
-    //
-    return {
-      topLeft: {
-        x: x,
-        y: y,
-        opposite: 'bottomRight'
-      },
-      topRight: {
-        x: x + width, 
-        y: y,
-        opposite: 'bottomLeft'
-      },
-      bottomRight: {
-        x: x + width,
-        y: y + height,
-        opposite: 'topLeft'
-      },
-      bottomLeft: {
-        x: x,
-        y: y + height,
-        opposite: 'topRight'
-      }
-    };
-  };
 
   componentDidMount() {
+    
+    const {
+      canvas
+    } = this.refs;
+
+    const {
+      width,
+      height
+    } = this.props;
+    HiDPICanvas(canvas, width, height);
+  }
+
+  handleSave(e) {
+    return;
+    const image = document.createElement('img');
+    image.setAttribute('src', this.getBlob());
+    document.body.appendChild(image);
+  }
+
+  handleFileChange(e) {
+    this.setState({
+      imageSrc: URL.createObjectURL(e.target.files[0])
+    });
+  }
+
+  handleFileLoad(e) {
+    this.paintCanvas({
+      x:0,
+      y:0,
+      width:200,
+      height:200,
+      image: e.target
+    })
+  }
+
+  paintCanvas(params) {
+    const {
+      canvas
+    } = this.refs;
+
+    const ctx = canvas.getContext('2d');
+    //
+    const temp = params || {};
+    //
+    const x = temp.x;
+    const y = temp.y;
+    const width = temp.width;
+    const height = temp.height;
+    const image = temp.image;
+    //  Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //  Setup styles
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'red';
+    //  Draw image
+    ctx.drawImage(image, x, y, width, height);
+  };
+
+  getBlob() {
     const canvas = this.refs.canvas;
-    HiDPICanvas(canvas, canvas.width, canvas.height);
+    //  Paint canvas sans bounding box
+    paintCanvas({ noBoundingBox: true });
+    //  Set blob
+    var blob = canvas.toDataURL();
+    //  Paint canvas with bounding box and anchors
+    paintCanvas();
+    //  Return blob
+    return blob;
   }
 
   render() {
 
-    const props = {} = this.props;
+    const {
+      handleSave,
+      handleFileChange,
+      handleFileLoad
+    } = this;
+
+    const {
+      width,
+      height
+    } = this.props;
+
+    const {
+      imageSrc
+    } = this.state;
 
     return (
-      <canvas ref="canvas"/>
+      <div>
+        <canvas ref="canvas" width={width} height={height}/>
+        <img src={imageSrc} onLoad={handleFileLoad} width="0" height="0" />
+        <br />
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleSave}>Save</button>
+      </div>
     );
   }
 };
--- app/assets/javascripts/index.jsx
+++ app/assets/javascripts/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AvatarEditor from './AvatarEditor/AvatarEditor';
-ReactDOM.render(<AvatarEditor />, document.getElementById('avatarEditor'));
+ReactDOM.render(<AvatarEditor width="300" height="300" />, document.getElementById('avatarEditor'));
